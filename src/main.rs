f34da09ac755108f8503761844d4e6372b8edfd8
Danilo Bargen <mail@dbrgn.ch>
2015-11-26 01:07:38 +0100

    Make bot initialization a bit nicer

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,9 @@ mod types;
 mod errors;
 mod commands;
 
-use telegram_bot::{Api, ListeningMethod, MessageType, ListeningAction};
+use std::process::exit;
+
+use telegram_bot::{Api, Listener, ListeningMethod, MessageType, ListeningAction};
 use threadpool::ThreadPool;
 use conv::TryFrom;
 
@@ -16,26 +18,44 @@ use types::Command;
 use commands::CommandHandler;
 
 
+/// Initialize and return a `telegram_bot::Listener` instance.
+fn get_listener() -> Listener {
+    let api = Api::from_env("TELEGRAM_BOT_TOKEN").unwrap();
+    match api.get_me() {
+        Ok(user) => println!("Starting {}...", user.first_name),
+        Err(e) => {
+            println!("Error: Could not fetch information about Telegram bot.");
+            println!("  Error Details: {:?}", e);
+            println!("  Maybe check your TELEGRAM_BOT_TOKEN env var?");
+            exit(1);
+        },
+    }
+    api.listener(ListeningMethod::LongPoll(None))
+
+}
+
+
 fn main() {
 
     // Initialize env logger
     env_logger::init().unwrap();
 
-    let api = Api::from_env("TELEGRAM_BOT_TOKEN").unwrap();
-    println!("getMe: {:?}", api.get_me());
-
-    // We want to listen for new updates via LongPoll
-    let mut listener = api.listener(ListeningMethod::LongPoll(None));
+    // Get Telegram Api listener
+    let mut listener = get_listener();
 
     // Create thread pool for command handlers
     let pool = ThreadPool::new(12);
 
     // Fetch new updates via long poll method
     listener.listen(|u| {
+
+        // Dispatch messages
         if let Some(m) = u.message {
 
+            // Process text messages
             if let MessageType::Text(text) = m.msg {
 
+                // Dispatch command handlers
                 let command = Command::try_from(&*text);
                 match command {
                     Ok(cmd) => {
