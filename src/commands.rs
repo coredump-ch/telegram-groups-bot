b2fc189f51b37c5ef2586b6a5f18d4e689ba1f60
Danilo Bargen <mail@dbrgn.ch>
2015-12-22 09:01:20 +0100

    More documentation

--- src/commands.rs
+++ src/commands.rs
@@ -3,14 +3,16 @@
 use types::Command;
 
 
+/// Log the command, don't do anything else.
 pub fn handle_log(command: &Command) -> Option<String> {
     info!("Handled command: {}", &command);
     None
 }
 
 
+/// Return help output.
 pub fn handle_help(command: &Command) -> Option<String> {
-    info!("Handled help: {}", command);
+    info!("Handled /help: {}", command);
     Some("Available commands:\n\n \
           /help - show this help\n \
           /groups - list all available groups, along with the invite link"
@@ -19,6 +21,7 @@ pub fn handle_help(command: &Command) -> Option<String> {
 }
 
 
+/// Return list of groups.
 pub fn handle_groups(command: &Command) -> Option<String> {
     info!("Handled /groups: {}", command);
     Some("Not yet implemented.".into())
